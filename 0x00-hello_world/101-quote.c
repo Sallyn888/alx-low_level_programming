9e65e2cd6811bfc9a9a1bfc389d1d05715bc1b3e
Sallyn888 <sallyniragire8@gmail.com>
2023-03-16 15:46:59 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Main function
+ * Return 1: means no error
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n-, stdout);
+	return (1);
+}
