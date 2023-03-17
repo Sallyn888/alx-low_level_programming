8f0f8c23d7cf07fb8d44f4251710d53f66974af2
Sallyn888 <sallyniragire8@gmail.com>
2023-03-17 12:35:41 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,19 +8,12 @@ int main(void)
 {
 	char c;
 
-	char d;
-
 	c = 'a';
-	d = 'A';
 	while
 		(c <= 'z') {
 			putchar(c);
 			c++;
 		}
-	while
-		(d <= 'Z') {
-			putchar(d);
-			d++;
-		}
+	putchar('\n');
 	return (0);
 }
